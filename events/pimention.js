119d563f354a8a2f97e13e6fb800429104294e4b
justusglasgow <jglasgow.jag3@gmail.com>
2025-05-18 18:45:16 -0400

    may18th2

---
+++ events/pimention.js
@@ -0,0 +1,14 @@
+const {Events} = require('discord.js');
+const {clientId} = require('../config.json');
+
+module.exports = {
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message){
+    let msg = message.content;
+    msg = msg.toLowerCase();
+    if( msg.search("pi") != -1 && message.author.id != clientId){
+      await message.channel.send("Did someone mention pi\?\?\!\!");
+    }
+  },
+};
