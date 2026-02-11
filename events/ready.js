5e313a84c393ab8bd9881cb7e1fa34c8525a49e0
justusglasgow <jglasgow.jag3@gmail.com>
2026-02-10 22:40:22 -0500

    nickname change

--- events/ready.js
+++ events/ready.js
@@ -42,15 +42,15 @@ module.exports = {
       }
     }
 
-    let bday = new cron.CronJob('06 14 20 16 8 *', ()=>{
+    let bday = new cron.CronJob('00 14 20 16 8 *', ()=>{
       userQ.setNickname(`${birthday}`);
       isHoliday = false;
       holidayCount = 0;
     });
-    let xmas = new cron.CronJob('00 14 20 6-25 12 *', holidayJob(christmas));
-    let hween = new cron.CronJob('01 14 20 12-31 10 *', holidayJob(halloween));
-    let vday = new cron.CronJob('0 38 22 8-14 2 *', holidayJob(valentines));
-    let eggDay = new cron.CronJob('03 14 20 1,2 4 *', holidayJob(easter));
+    let xmas = new cron.CronJob('01 14 20 6-25 12 *', holidayJob(christmas));
+    let hween = new cron.CronJob('02 14 20 12-31 10 *', holidayJob(halloween));
+    let vday = new cron.CronJob('03 14 20 8-14 2 *', holidayJob(valentines));
+    let eggDay = new cron.CronJob('04 14 20 1,2 4 *', holidayJob(easter));
     bday.start();
     xmas.start();
     hween.start();
